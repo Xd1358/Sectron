2f8dadd593e6d9d2750b8a18b7609e5bc2612a3d
Mika Suomalainen <mika.henrik.mainio@hotmail.com>
2011-08-15 16:55:40 +0300

    Translated new strings and updated old.
    Also added lang/en.py which seemed to be missing.

--- lang/fi.py
+++ lang/fi.py
@@ -3,4 +3,9 @@
 IDENTIFY_SUCCESS = "Olet nyt tunnistautunut käyttäjäksi \x02%s\x02"
 REGISTER_SUCCESS = "Kiitos rekisteröimisestä."
 REGISTER_E_EMAIL = "Ole hyvä ja anna kelvollinen sähköpostiosoite."
-IS_ALREADY_REGGED = "\x02%s\x02 on jo rekisteröitynyt."
+IS_ALREADY_REGGED = "\x02%s\x02 on jo rekisteröity käyttäjätunnus."
+INVALID_PASSOWRD = "Väärä salasana."
+NOW_LOGGEDOUT = "Olet nyt kirjautunut ulos."
+
+NO_PERMS = "Sinun ei ole sallittu suorittaa tätä operaatiota." # Unclear translation.
+VHOST_SUCCESS = "Valitulle tunnukselle on nyt asetettu VHOST."
