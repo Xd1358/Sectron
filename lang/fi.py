b7f0f39ab5094cd8afdf1abce1177317577db645
Xd1358 <xd1358.jedipedia@hotmail.com>
2011-08-15 18:03:53 +0300

    test

--- lang/fi.py
+++ lang/fi.py
@@ -1,11 +1,11 @@
 # Finnish Language Pack for Sectron
 
 IDENTIFY_SUCCESS = "Olet nyt tunnistautunut käyttäjäksi \x02%s\x02"
-REGISTER_SUCCESS = "Kiitos rekisteröimisestä."
+REGISTER_SUCCESS = "Kiitos rekisteröitymisestä."
 REGISTER_E_EMAIL = "Ole hyvä ja anna kelvollinen sähköpostiosoite."
 IS_ALREADY_REGGED = "\x02%s\x02 on jo rekisteröity käyttäjätunnus."
 INVALID_PASSOWRD = "Väärä salasana."
 NOW_LOGGEDOUT = "Olet nyt kirjautunut ulos."
 
-NO_PERMS = "Sinun ei ole sallittu suorittaa tätä operaatiota." # Unclear translation.
+NO_PERMS = "Sinun ei ole oikeuksia suorittaa tätä toimintoa."
 VHOST_SUCCESS = "Valitulle tunnukselle on nyt asetettu VHOST."
